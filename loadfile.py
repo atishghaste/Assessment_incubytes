3320980c4277023d69736d385f8fe8aa85ee3dd0
Atish Ghaste <85488317+atishghaste@users.noreply.github.com>
2021-10-18 00:45:43 +0530

    touch

--- loadfile.py
+++ loadfile.py
@@ -1,11 +1,11 @@
-import psycopg2 as pg
+import psycopg2 as pg                  #import libraries
 import pandas as pd
 
-conn = pg.connect(database="postgres", user="postgres", password="12345", host="127.0.0.1", port="5432")
+conn = pg.connect(database="postgres", user="postgres", password="12345", host="127.0.0.1", port="5432")  
 
-df = pd.read_sql_query('select * from patients',con=conn)
+df = pd.read_sql_query('select * from patients',con=conn)   #read file
 
 print(df)
 
-ans = df.loc[df['country'] == "IND"]
+ans = df.loc[df['country'] == "IND"]      #seperation of IND categorywise 
 print(ans)
